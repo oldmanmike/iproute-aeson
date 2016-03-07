21a6c4ee0232113a52b99e6f30d96aa4b681be43
oldmanmike <oldmanmike.dev@gmail.com>
2016-03-06 20:18:45 -0500

    Clean up some stuff

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,36 +1,63 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+import            Control.Monad
+import            Data.IP
+import            Data.Aeson
+import            Data.IP.Aeson
+import            Data.Word
+import            GHC.Generics
+import            Test.QuickCheck
 
-import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
-import Control.Monad
-import System.Random
-import Data.IP
-import Data.Aeson
-import Data.IP.Aeson
 
-import qualified Data.ByteString.Lazy as B
+data ExtractedIPv4 = ExtractedIPv4
+  { ipv4:: IPv4
+  } deriving (Show,Eq,Generic)
 
-rndIPv4 :: StdGen -> IPv4
-rndIPv4 gen = toIPv4 ip
-    where ip = rndSelect gen [0..256] 4 :: [Int]
 
-rndSelect :: StdGen -> [a] -> Int -> [a]
-rndSelect gen lst i = take i [ lst !! x | x <- randomRs (0, (length lst) - 1) gen]
+data ExtractedIPv6 = ExtractedIPv6
+  { ipv6 :: IPv6
+  } deriving (Show,Eq,Generic)
 
--- 4 Ints
+
+instance ToJSON ExtractedIPv4
+instance FromJSON ExtractedIPv4
+instance ToJSON ExtractedIPv6
+instance FromJSON ExtractedIPv6
+
+
 instance Arbitrary IPv4 where
   arbitrary = do
-    let a = choose (0,255)
-    let b = choose (0,255)
-    let c = choose (0,255)
-    let d = choose (0,255)
+    let a = fmap fromEnum (arbitrary :: Gen Word8)
+    let b = fmap fromEnum (arbitrary :: Gen Word8)
+    let c = fmap fromEnum (arbitrary :: Gen Word8)
+    let d = fmap fromEnum (arbitrary :: Gen Word8)
     liftM toIPv4 (sequence [a,b,c,d])
 
 
-prop_parseJSONIPv4 :: IPv4 -> Property
-prop_parseJSONIPv4 ip = True ==>
-  (Just ip) == decode ("{\"ip\": " `B.append` (encode ip) `B.append` " }")
+instance Arbitrary IPv6 where
+  arbitrary = do
+    let a = fmap fromEnum (arbitrary :: Gen Word16)
+    let b = fmap fromEnum (arbitrary :: Gen Word16)
+    let c = fmap fromEnum (arbitrary :: Gen Word16)
+    let d = fmap fromEnum (arbitrary :: Gen Word16)
+    let e = fmap fromEnum (arbitrary :: Gen Word16)
+    let f = fmap fromEnum (arbitrary :: Gen Word16)
+    let g = fmap fromEnum (arbitrary :: Gen Word16)
+    let h = fmap fromEnum (arbitrary :: Gen Word16)
+    liftM toIPv6 (sequence [a,b,c,d,e,f,g,h])
+
+
+prop_IdentityJSONIPv4 :: IPv4 -> Bool
+prop_IdentityJSONIPv4 ip = (Just testData) == decode (encode testData)
+  where testData = ExtractedIPv4 ip
+
+
+prop_IdentityJSONIPv6 :: IPv6 -> Bool
+prop_IdentityJSONIPv6 ip = (Just testData) == decode (encode testData)
+  where testData = ExtractedIPv6 ip
+
 
 main :: IO ()
-main = quickCheck prop_parseJSONIPv4
+main = do
+  quickCheck prop_IdentityJSONIPv4
+  quickCheck prop_IdentityJSONIPv6
